50fd26ef73ae6a52a468f862d59819d7564ca7c0
2lz4m3 <157938150+2lz4m3@users.noreply.github.com>
2024-02-12 06:13:52 +0000

    Add test case

--- run/badrodem_test.go
+++ run/badrodem_test.go
@@ -1,7 +1,31 @@
 package run
 
-import "testing"
+import (
+	"testing"
+)
 
-func Test(t *testing.T) {
-	t.Skip()
+func TestIsProbablyText(t *testing.T) {
+	testCases := []struct {
+		name     string
+		input    []byte
+		expected bool
+	}{
+		{
+			name:     "string a",
+			input:    []byte("a"),
+			expected: true,
+		},
+		{
+			name:     "binary 0x01",
+			input:    []byte{0x01},
+			expected: false,
+		},
+	}
+
+	for _, testCase := range testCases {
+		got := isProbablyText(testCase.input)
+		if got != testCase.expected {
+			t.Errorf("got:%v, %+v", got, testCase)
+		}
+	}
 }
